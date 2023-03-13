fce65a0ae7c39d881cd0b26108e3c0d0b3148178
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-03-13 12:12:29 -0400

    first init

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Container>Header</Container>
+    )
+}
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13;
+`
+
+export default Header;
+
